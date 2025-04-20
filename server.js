c7b2f88d1acf64d8bd032364f4f3812df977baad
azputra <ahmadzp102@gmail.com>
2025-04-20 18:55:56 +0700

    update port

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
