fa4fd7ba3d68999efd374fbf2941804e01eb3ebc
azputra <ahmadzp102@gmail.com>
2025-04-21 21:18:44 +0700

    test be cert

--- controllers/certificateController.js
+++ controllers/certificateController.js
@@ -447,7 +447,7 @@ const generateCertificatePDF = async (req, res) => {
     
     // Buat PDF dengan Puppeteer
     const browser = await puppeteer.launch({
-      headless: 'new'
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     
     const page = await browser.newPage();
