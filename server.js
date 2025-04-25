100499fa485698fae5350d8465e20970d53fa6fd
azputra <ahmadzp102@gmail.com>
2025-04-25 15:23:35 +0700

    fix cert

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://secuone.netlify.app'], // sesuaikan dengan URL frontend
+  origin: [
+    'http://localhost:5173', 
+    'https://secuone.netlify.app',
+    'https://sertifikat-backend.onrender.com' // Tambahkan ini
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
